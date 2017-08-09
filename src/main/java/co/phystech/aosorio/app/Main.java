c12e19a6e99a0eebda0e8a3fe37c8cf79ac3f93a
andres0sorio <osorio.af@gmail.com>
2017-08-08 20:21:48 -0500

    [File] list directory content

--- src/main/java/co/phystech/aosorio/app/Main.java
+++ src/main/java/co/phystech/aosorio/app/Main.java
@@ -2,18 +2,47 @@ package co.phystech.aosorio.app;
 
 import static spark.Spark.*;
 
+import java.io.File;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
-    public static void main(String[] args) {
-    	port(getHerokuAssignedPort());
-        get("/hello", (req, res) -> "Hello World");
-    }
-    
-    static int getHerokuAssignedPort() {
-        ProcessBuilder processBuilder = new ProcessBuilder();
-        if (processBuilder.environment().get("PORT") != null) {
-            return Integer.parseInt(processBuilder.environment().get("PORT"));
-        }
-        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
-    }
+
+	private final static Logger slf4jLogger = LoggerFactory.getLogger(Main.class);
+
+	public static void main(String[] args) {
+
+		port(getHerokuAssignedPort());
+
+		get("/hello", (req, res) -> "Hello World");
+
+		try {
+
+			File dir = new File(".");
+			File[] filesList = dir.listFiles();
+			for (File file : filesList) {
+				if (file.isFile()) {
+					slf4jLogger.info(file.getName());
+				}
+			}
+			slf4jLogger.info("Gresat work!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			slf4jLogger.info("Problem extracting Properties file");
+		}
+
+		slf4jLogger.info("Program is working fine");
+
+	}
+
+	static int getHerokuAssignedPort() {
+		ProcessBuilder processBuilder = new ProcessBuilder();
+		if (processBuilder.environment().get("PORT") != null) {
+			return Integer.parseInt(processBuilder.environment().get("PORT"));
+		}
+		return 4567;
+	}
 
 }
